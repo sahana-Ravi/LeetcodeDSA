697464f181a60965543b267582cba397caa33b8d
sahana-Ravi <93814845+sahana-Ravi@users.noreply.github.com>
2025-04-17 21:26:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (74.53%) - LeetSync

--- Matrix/3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ Matrix/3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,8 +1,13 @@
 class Solution {
+    public int powerOf2(int n){
+        int p = 1;
+        while(p<n){
+            p*=2;
+        }
+        return p;
+    }
     public int smallestNumber(int n) {
-         int res = 1;
-        while (res < n)
-            res = res * 2 + 1;
-        return res;
+        int num = powerOf2(n+1);
+        return num-1;
     }
 }
