92acf5271f560bcc3b1d05a6c557e4e070481e86
sahana-Ravi <93814845+sahana-Ravi@users.noreply.github.com>
2025-04-17 10:39:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (13.61%) - LeetSync

--- Strings/908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ Strings/908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -8,19 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode cur = head;
+//         int c =0;
+//         while(cur!=null){
+//            c+=1;
+//            cur=cur.next;
+//         }
+//         cur = head;
+//         for(int i=0;i<(c/2);i++){
+//              cur = cur.next;
+//         }
+//         return cur;
+        
+//     }
+// }
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode cur = head;
-        int c =0;
-        while(cur!=null){
-           c+=1;
-           cur=cur.next;
+        ListNode s1=head;
+        ListNode s2=head;
+
+        while(s2!=null && s2.next!=null){
+            s1=s1.next;
+            s2=s2.next.next;
         }
-        cur = head;
-        for(int i=0;i<(c/2);i++){
-             cur = cur.next;
-        }
-        return cur;
-        
+        return s1;
     }
 }
